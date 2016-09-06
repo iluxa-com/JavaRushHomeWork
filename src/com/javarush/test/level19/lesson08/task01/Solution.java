bd27b71fa1286b301ce79a5037f3b971af6961c7
iluxa.com <iluxa_com@mail.ru>
2016-09-06 23:41:33 +0300

    19.8.1 test passed

---
+++ src/com/javarush/test/level19/lesson08/task01/Solution.java
@@ -0,0 +1,39 @@
+package com.javarush.test.level19.lesson08.task01;
+
+/* Ридер обертка
+В методе main подмените объект System.out написанной вами ридер-оберткой по аналогии с лекцией
+Ваша ридер-обертка должна преобразовывать весь текст в заглавные буквы
+Вызовите готовый метод printSomething(), воспользуйтесь testString
+Верните переменной System.out первоначальный поток.
+Вывести модифицированную строку в консоль.
+*/
+
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+
+public class Solution {
+    public static TestString testString = new TestString();
+
+    public static void main(String[] args) {
+
+        PrintStream systemStream = System.out;
+        ByteArrayOutputStream arrayOutputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(arrayOutputStream);
+
+        System.setOut(printStream);
+
+        testString.printSomething();
+
+        System.setOut(systemStream);
+
+        System.out.println(arrayOutputStream.toString().toUpperCase());
+
+    }
+
+    public static class TestString {
+        public void printSomething() {
+            System.out.println("it's a text for testing");
+        }
+    }
+}
